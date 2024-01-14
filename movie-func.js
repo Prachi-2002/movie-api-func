467866cd32774bf8938f777716573a22e92a3967
Prachi-2002 <prachimadame01@gmail.com>
2024-01-14 20:52:55 +0530

    added the api call

--- movie-func.js
+++ movie-func.js
@@ -1,19 +1,10 @@
-// netlify/functions/proxy-omdb.js
-
-const headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type',
-  };
-  
-
 const axios = require('axios');
 
-exports.handler = async (val="batman", page=1) => {
+exports.handler = async (val = "batman", page = 1) => {
   try {
     const response = await axios.get(`http://www.omdbapi.com/?apiKey=ff24d901&s=${val}&page=${page}`);
     return {
       statusCode: 200,
-      headers,
       body: JSON.stringify(response.data),
     };
   } catch (error) {
